f6b5895ba3079a67a764e085ef389fee70b3ffb5
anas khan <53373313+ak47ak47@users.noreply.github.com>
2022-07-27 02:11:51 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signOut, signInWithPopup, onAuthStateChanged, GoogleAuthProvider, signInWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, collection, onSnapshot, addDoc, setDoc, updateDoc, deleteDoc, doc, query, orderBy, serverTimestamp } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDuM5ZzIc9FC-CiSBMyNWM8Ru4XeHDz2io",
-    authDomain: "sandese-app.firebaseapp.com",
-    projectId: "sandese-app",
-    storageBucket: "sandese-app.appspot.com",
-    messagingSenderId: "379805090136",
-    appId: "1:379805090136:web:15b91aedf60157455f252d",
-    measurementId: "G-8HD1CPNFFS"
+  apiKey: "AIzaSyBN8VH2S74uiWk7ndgS4JxGAxXzEHnmygI",
+  authDomain: "bittorex-5479b.firebaseapp.com",
+  projectId: "bittorex-5479b",
+  storageBucket: "bittorex-5479b.appspot.com",
+  messagingSenderId: "672435159761",
+  appId: "1:672435159761:web:b4c2a9301c9ace4f52bbf0",
+  measurementId: "G-C5WFQJZVWT"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
